a649e0cf29ffeadc058a9cb227fb2bc0b5a3a80a
ANDRESWEB <andresamorin1@gmail.com>
2022-12-13 15:20:25 -0300

    nuevo commit

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,12 +1,33 @@
 import ItemDetail from "../ItemDetail/ItemDetail"
-import { Productos } from "../Productos/Productos"
+import { useState } from "react";
+import { Productos, categorias } from "../Productos/Productos";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
-const ItemDetailContainer = () => {
-    return (
-        <div>
-            <ItemDetail producto={producto} />
-        </div>
-    )
+const ItemDetailtContainer = () => {
+   const [ItemDetail,setItemDetail] = useState(Productos);
+   const { id } = useParams()
+
+   const FilterProduct = new Promise((resolve,eject)=>{
+    const newProduct = Productos.filter((p)=>p.id == id)
+    resolve(newProduct)
+   })
+
+
+   useEffect(()=>{
+    FilterProduct.then((response)=>{
+        setItemDetail(response)
+    })
+   },[id])
+   return(
+    <div>
+        {
+            ItemDetail && ItemDetail.map((producto)=>{
+                return <ItemDetail producto={producto} />
+            })
+        }
+    </div>
+   )
 }
 
-export default ItemDetailContainer
+export default ItemDetailtContainer;
